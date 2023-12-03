25edc25bde8ea34a6fc7c6c592144eac4419b671
shark0a <shar40951@gmail.com>
2023-12-03 22:48:11 +0200

    init hive and notesbox

--- lib/helper/const.dart
+++ lib/helper/const.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
-Color pcolor =const Color(0xFF000000);
-Color fcolor = const Color(0xff62FCD7);
+const Color pcolor =const Color(0xFF000000);
+const Color fcolor = const Color(0xff62FCD7);
+const knotesbox='nots_box';
